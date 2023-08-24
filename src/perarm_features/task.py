7a522906fda5be33a5220c3a02d0c5a0ab908f37
jordan <jv.tott@gmail.com>
2023-08-24 15:01:22 +0000

    fix eval bug

--- src/perarm_features/task.py
+++ src/perarm_features/task.py
@@ -60,6 +60,8 @@ nest = tf.nest
 from . import train_perarm as train_perarm
 from . import agent_factory as agent_factory
 from . import reward_factory as reward_factory
+from . import eval_perarm as eval_perarm
+from . import emb_features as emb_features
 
 from src.per_arm_rl import data_utils
 from src.per_arm_rl import train_utils
@@ -74,12 +76,6 @@ PER_ARM = True  # Use the non-per-arm version of the MovieLens environment.
 # clients
 project_number = os.environ["CLOUD_ML_PROJECT_ID"]
 storage_client = storage.Client(project=project_number)
-
-# vertex_ai.init(
-#     project=project_number,
-#     location='us-central1',
-#     experiment=args.experiment_name
-# )
 
 # ====================================================
 # get train & val datasets
@@ -297,7 +293,6 @@ def main(args: argparse.Namespace):
     # ====================================================
     # trajectory_fn
     # ====================================================
-    from . import emb_features as emb_features
     
     embs = emb_features.EmbeddingModel(
         vocab_dict = VOCAB_DICT,
@@ -354,90 +349,6 @@ def main(args: argparse.Namespace):
         )
     
     # ====================================================
-    # run bandit eval
-    # ====================================================
-    def _run_bandit_eval(
-        policy,
-        data,
-        eval_batch_size,
-        per_arm_dim,
-        global_dim,
-    ):
-        logged_rewards = []
-        predicted_rewards = []
-        trouble_list = []
-        train_loss_results = []
-
-        dummy_arm = tf.zeros([eval_batch_size, per_arm_dim], dtype=tf.float32)
-
-        for x in data:
-            # get feature tensors
-            
-            # global_feat_infer = _get_global_context_features(x)
-            # arm_feat_infer = _get_per_arm_features(x)
-            
-            global_feat_infer = embs._get_global_context_features(x)
-            arm_feat_infer = embs._get_per_arm_features(x)
-            
-            rewards = reward_factory._get_rewards(x)
-
-            # reshape arm features
-            arm_feat_infer = tf.reshape(arm_feat_infer, [eval_batch_size, per_arm_dim])
-            concat_arm = tf.concat([arm_feat_infer, dummy_arm], axis=0)
-
-            # flatten global
-            flat_global_infer = tf.reshape(global_feat_infer, [global_dim])
-            feature = {'global': flat_global_infer, 'per_arm': concat_arm}
-
-            # get actual reward
-            actual_reward = rewards.numpy()[0]
-            # logged_rewards.append(actual_reward)
-
-            # build trajectory step
-            trajectory_step = train_utils._get_eval_step(feature, actual_reward)
-
-            # pred w/ trained agent
-            prediction = policy.action(trajectory_step)
-
-            predicted_rewards_mean = prediction.info.predicted_rewards_mean #[0]
-            # pred_rewards_mean_list.append(predicted_rewards_mean)
-
-            predicted_reward_tf = tf.gather(
-                predicted_rewards_mean,
-                prediction.action, 
-                batch_dims=0, 
-                axis=-1
-            )
-            pred_reward = float(round(predicted_reward_tf.numpy()))
-
-            # When the uniform random policy is used, the loss is meaningless for evaluation
-            # > discard preds from uniform random policy
-            # > keep preds from greedy policy, 
-            # TODO: replace hard-coded values with parameterized filter/mask
-            if pred_reward < 0:
-                trouble_list.append(pred_reward)
-            elif pred_reward > 5:
-                trouble_list.append(pred_reward)
-            else:
-                predicted_rewards.append(pred_reward)
-
-                pred_loss = tf.keras.metrics.mean_squared_error(
-                    rewards, predicted_reward_tf
-                )
-                train_loss_results.append(pred_loss)
-                logged_rewards.append(actual_reward)
-
-        # calculate avg loss
-        avg_eval_loss = tf.reduce_mean(train_loss_results)
-
-        return (
-            avg_eval_loss,
-            predicted_rewards,
-            logged_rewards,
-            # train_loss_results
-        )
-    
-    # ====================================================
     # create agent
     # ====================================================
     
@@ -462,9 +373,6 @@ def main(args: argparse.Namespace):
         observation_spec = observation_spec, 
     )
     print(f"time_step_spec: {time_step_spec}")
-    
-    # TODO - cleanup
-    from . import agent_factory as agent_factory
 
     with distribution_strategy.scope():
 
@@ -586,12 +494,16 @@ def main(args: argparse.Namespace):
     print(f"evaluating trained Policy...")
     start_time = time.time()
 
-    val_loss, preds, tr_rewards = _run_bandit_eval(
+    val_loss, preds, tr_rewards = eval_perarm._run_bandit_eval(
         policy = trained_policy,
         data = eval_ds, # eval_ds | dist_eval_ds
         eval_batch_size = args.eval_batch_size,
         per_arm_dim = args.per_arm_dim,
-        global_dim = args.global_dim
+        global_dim = args.global_dim,
+        vocab_dict = VOCAB_DICT,
+        num_oov_buckets = args.num_oov_buckets,
+        global_emb_size = args.global_emb_size,
+        mv_emb_size = args.mv_emb_size,
     )
 
     runtime_mins = int((time.time() - start_time) / 60)
