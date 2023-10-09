e9d2d872cffd66cb607faea85efb35f3a1d884f7
jordan <jv.tott@gmail.com>
2023-10-09 17:42:10 +0000

    fix int64 list feature parsing

--- src/per_arm_rl/data_config.py
+++ src/per_arm_rl/data_config.py
@@ -2,7 +2,7 @@
 USER_AGE_LOOKUP       = {1.0: 0, 35.0: 1, 45.0: 2, 18.0: 3, 50.0: 4, 56.0: 5, 25.0: 6}
 USER_AGE_DIM          = 7
 
-USER_OCC_LOOKUP       = {b'other': 0, b'entertainment': 1, b'writer': 2, b'healthcare': 3, b'none': 4, b'retired': 5, b'student': 6, b'executive': 7, b'educator': 8, b'marketing': 9, b'librarian': 10, b'doctor': 11, b'salesman': 12, b'engineer': 13, b'lawyer': 14, b'homemaker': 15, b'programmer': 16, b'technician': 17, b'administrator': 18, b'scientist': 19, b'artist': 20}
+USER_OCC_LOOKUP       = {b'lawyer': 0, b'engineer': 1, b'other': 2, b'homemaker': 3, b'none': 4, b'retired': 5, b'doctor': 6, b'programmer': 7, b'salesman': 8, b'scientist': 9, b'student': 10, b'technician': 11, b'healthcare': 12, b'writer': 13, b'educator': 14, b'marketing': 15, b'executive': 16, b'artist': 17, b'librarian': 18, b'administrator': 19, b'entertainment': 20}
 USER_OCC_DIM          = 21
 
 MOVIE_GEN_LOOKUP      = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 12: 11, 13: 12, 14: 13, 15: 14, 16: 15, 17: 16, 18: 17, 19: 18}
