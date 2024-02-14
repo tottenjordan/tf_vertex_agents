e4fdfbe748002db56feb8f3026844aa1e7d83542
jordan <jordantotten@google.com>
2024-02-14 16:00:07 +0000

    updated data config

---
+++ src/per_arm_rl/data_config_1m.py
@@ -0,0 +1,12 @@
+
+USER_AGE_LOOKUP       = {1.0: 0, 35.0: 1, 45.0: 2, 18.0: 3, 50.0: 4, 56.0: 5, 25.0: 6}
+USER_AGE_DIM          = 7
+
+USER_OCC_LOOKUP       = {b'clerical/admin': 0, b'other/not specified': 1, b'doctor/health care': 2, b'college/grad student': 3, b'writer': 4, b'lawyer': 5, b'tradesman/craftsman': 6, b'scientist': 7, b'farmer': 8, b'technician/engineer': 9, b'programmer': 10, b'unemployed': 11, b'K-12 student': 12, b'executive/managerial': 13, b'sales/marketing': 14, b'self-employed': 15, b'academic/educator': 16, b'retired': 17, b'customer service': 18, b'artist': 19, b'homemaker': 20}
+USER_OCC_DIM          = 21
+
+MOVIE_GEN_LOOKUP      = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 12: 11, 13: 12, 14: 13, 15: 14, 16: 15, 18: 16, 19: 17}
+MOVIE_GEN_DIM         = 18
+
+MOVIELENS_NUM_MOVIES  = 3707
+MOVIELENS_NUM_USERS   = 6041
